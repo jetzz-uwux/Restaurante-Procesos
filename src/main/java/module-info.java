a78277afa992bd5fac6dab19d4a00fd0bebadc3e
jetzz-uwux <jetzaly2006@gmail.com>
2026-04-13 07:50:43 -0600

    Carpetas FXML, paquete de controllers y CSS

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module progs.restaurante {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens progs.restaurante to javafx.fxml;
     exports progs.restaurante;
