5c003a5133a046d53df19c71d1d7654714668a49
JuanGaona-15 <zs24013373@estudiantes.uv.mx>
2026-05-06 13:40:09 -0600

    Actualizado

--- src/main/java/progs/restaurante/datos/ConexionBD.java
+++ src/main/java/progs/restaurante/datos/ConexionBD.java
@@ -9,7 +9,7 @@ public class ConexionBD {
 
     private static final String URL = "jdbc:mysql://localhost:3306/restaurante";
     private static final String USER = "root";
-    private static final String PASS = "Jetzalytc01*";
+    private static final String PASS = "recuerden cambiar esto por el suyo";
     
     /* private static final String URL = "jdbc:mysql://localhost:3306/restaurante";
     private static final String USER = "root";
