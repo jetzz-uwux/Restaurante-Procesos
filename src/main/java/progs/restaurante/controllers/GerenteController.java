18038ebb4a052073624860c4c66b001db402db2c
JuanGaona-15 <zs24013373@estudiantes.uv.mx>
2026-05-11 07:41:32 -0600

    Ventana gerente

---
+++ src/main/java/progs/restaurante/controllers/GerenteController.java
@@ -0,0 +1,79 @@
+package progs.restaurante.controllers;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class GerenteController {
+
+    @FXML
+    private Button btn_ayuda;
+
+    @FXML
+    private Button btn_cerrarsesion;
+
+    @FXML
+    private Button btn_reportes;
+
+    @FXML
+    private Button btn_inventario;
+
+    /**
+     * Abrir ventana de Reportes
+     */
+    @FXML
+    private void abrirReportes(ActionEvent event) {
+
+        try {
+
+            Parent root = FXMLLoader.load(
+                    getClass().getResource("/progs/fxml/VentanaVentas.fxml")
+            );
+
+            Stage stage = new Stage();
+            stage.setTitle("Reportes");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Abrir ventana de Inventario
+     */
+    @FXML
+    private void abrirInventario(ActionEvent event) {
+
+        try {
+
+            Parent root = FXMLLoader.load(
+                    getClass().getResource("/progs/fxml/AlmacenVista.fxml")
+            );
+
+            Stage stage = new Stage();
+            stage.setTitle("Inventario");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Cerrar sesión
+     */
+    @FXML
+    private void cerrarSesion(ActionEvent event) {
+
+        Stage stage = (Stage) btn_cerrarsesion.getScene().getWindow();
+        stage.close();
+    }
+}
--- src/main/java/progs/restaurante/datos/ConexionBD.java
+++ src/main/java/progs/restaurante/datos/ConexionBD.java
@@ -9,12 +9,8 @@ public class ConexionBD {
 
     private static final String URL = "jdbc:mysql://localhost:3306/restaurante";
     private static final String USER = "root";
-    private static final String PASS = "Jetzalytc01*";
+    private static final String PASS = "Gatitos1510";
 
-    /*    private static final String URL = "jdbc:mysql://localhost:3306/restaurante";
-    private static final String USER = "root";
-    private static final String PASS = "recuerden cambiar esto por el suyo";
-    */
     public static Connection getConexion() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
     }
